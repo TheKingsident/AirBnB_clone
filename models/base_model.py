eecb5b72e677f5fe0a158a7e3af38e6a1d7d6f26
TheKingsident <kingsident@gmail.com>
2023-11-08 21:04:37 +0100

    Added new files

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ Subsequent classes ma inherit from this
 """
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """
@@ -25,9 +25,20 @@ class BaseModel:
         Initializes a new instance of BaseModel, assigning id, created_at
         and updated_at.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -47,6 +58,7 @@ class BaseModel:
         datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -66,3 +78,4 @@ class BaseModel:
         model_dict['created_at'] = self.created_at.isoformat()
         model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
+     
---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+The Filestorage class handling both serialization and deserialization
+"""
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances."""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
+            FileStorage.__objects[key] = obj
+    
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {obj_id: obj.to_dict() for obj_id, obj in FileStorage.__objects.items()}
+        with open (FileStorage.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects if file exists, otherwise do nothing."""
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                objs = json.load(f)
+                for obj_id, obj_data in objs.items():
+                    cls_name = obj_data['__class__']
+                    del obj_data['__class__']
+                    cls = globals().get(cls_name)
+                    if cls:  # Ensure that cls is not None
+                        try:
+                            self.new(cls(**obj_data))
+                        except TypeError as e:
+                            print(f"TypeError when attempting to create {cls_name} with data {obj_data}: {e}")
+                        except Exception as e:
+                            print(f"Unexpected error when creating {cls_name}: {e}")
+                    else:
+                        print(f"Class {cls_name} not found in global scope.")
+        except FileNotFoundError:
+            print("JSON file not found.")
+        except json.JSONDecodeError as e:
+            print(f"JSON Decode Error: {e}")
