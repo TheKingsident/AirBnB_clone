9b3706cc5d5756865b57e1f3fcdc2064ad307c3d
TheKingsident <kingsident@gmail.com>
2023-11-12 16:00:40 +0100

    Added new files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """
 The Filestorage class handling both serialization and deserialization
 """
+from encodings import utf_8
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -19,26 +20,34 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
-        if obj:
-            key = obj.__class__.__name__ + '.' + obj.id
-            FileStorage.__objects[key] = obj
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
         obj_dict = {obj_id: obj.to_dict()
                     for obj_id, obj in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w', encoding='utf_8') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """Deserializes the JSON file to __objects if file exists, otherwise
         do nothing."""
         try:
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r', encoding='utf_8') as f:
                 objs = json.load(f)
                 for obj_id, obj_data in objs.items():
                     cls_name = obj_data['__class__']
-                    del obj_data['__class__']
+                    cls = globals()[cls_name]
+                    self.new(cls(**obj_data))
+        except FileNotFoundError:
+            pass
+        """
+        try:
+            with open(FileStorage.__file_path, 'r', encoding='utf_8') as f:
+                objs = json.load(f)
+                for obj_id, obj_data in objs.items():
+                    cls_name = obj_data['__class__']
                     cls = globals().get(cls_name)
                     if cls:  # Ensure that cls is not None
                         try:
@@ -53,3 +62,4 @@ class FileStorage:
             print("JSON file not found.")
         except json.JSONDecodeError as e:
             print(f"JSON Decode Error: {e}")
+        """
