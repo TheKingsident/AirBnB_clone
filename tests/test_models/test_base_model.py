49062fbbbf96c2d499c663385926bb70afcf03a7
TheKingsident <kingsident@gmail.com>
2023-11-12 17:02:03 +0100

    Added new files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,89 +4,109 @@ This module is a unittest for the BaseModel class
 """
 from models.base_model import BaseModel
 import unittest
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 class TestBaseModel(unittest.TestCase):
 
     def test_initialization(self):
-        """Test the initialization of BaseModel without kwargs."""
+        """Test BaseModel initialization without kwargs."""
         instance = BaseModel()
         self.assertIsNotNone(instance.id, "id should not be None")
-        self.assertTrue(isinstance(instance.created_at, datetime), "created_at should be a datetime object")
-        self.assertTrue(isinstance(instance.updated_at, datetime), "updated_at should be a datetime object")
+        self.assertTrue(isinstance(instance.created_at, datetime),
+                        "created_at should be datetime")
+        self.assertTrue(isinstance(instance.updated_at, datetime),
+                        "updated_at should be datetime")
 
         time_diff = instance.updated_at - instance.created_at
-        self.assertTrue(time_diff < timedelta(seconds=1), "created_at and updated_at should be almost equal at initialization")
+        self.assertTrue(time_diff < timedelta(seconds=1),
+                        "created_at & updated_at almost equal at init")
 
     def test_initialization_with_kwargs(self):
-        """Test the initialization of BaseModel with kwargs."""
+        """Test BaseModel initialization with kwargs."""
         kwargs = {
             'id': '123',
             'created_at': '2021-01-01T00:00:00.000000',
             'updated_at': '2021-01-02T00:00:00.000000'
         }
         instance = BaseModel(**kwargs)
-        self.assertEqual(instance.id, '123', "id should match the one provided in kwargs")
-        self.assertEqual(instance.created_at.isoformat(timespec='microseconds'), kwargs['created_at'], "created_at should match the one provided in kwargs")
-        self.assertEqual(instance.updated_at.isoformat(timespec='microseconds'), kwargs['updated_at'], "updated_at should match the one provided in kwargs")
+        self.assertEqual(instance.id, '123', "id matches provided in kwargs")
+        self.assertEqual(
+            instance.created_at.isoformat(timespec='microseconds'),
+            kwargs['created_at'], "created_at matches provided in kwargs")
+        self.assertEqual(
+            instance.updated_at.isoformat(timespec='microseconds'),
+            kwargs['updated_at'], "updated_at matches provided in kwargs")
 
     def test_string_representation(self):
         """Test the string representation of BaseModel."""
         instance = BaseModel()
         expected_str = f"[BaseModel] ({instance.id}) {instance.__dict__}"
-        self.assertEqual(instance.__str__(), expected_str, "String representation should match the expected format")
+        self.assertEqual(instance.__str__(), expected_str,
+                         "String representation matches expected format")
 
     def test_save_method(self):
         """Test the save method of BaseModel."""
         instance = BaseModel()
         old_updated_at = instance.updated_at
         instance.save()
-        self.assertNotEqual(instance.updated_at, old_updated_at, "updated_at should be updated after save")
+        self.assertNotEqual(instance.updated_at, old_updated_at,
+                            "updated_at updated after save")
 
     def test_dictionary_conversion(self):
         """Test the dictionary conversion of BaseModel."""
         instance = BaseModel()
         instance_dict = instance.to_dict()
-        self.assertEqual(instance_dict['__class__'], 'BaseModel', "__class__ key should be 'BaseModel'")
-        self.assertEqual(instance_dict['id'], instance.id, "id in dictionary should match instance's id")
-        self.assertIsInstance(instance_dict['created_at'], str, "created_at should be a string")
-        self.assertIsInstance(instance_dict['updated_at'], str, "updated_at should be a string")
+        self.assertEqual(instance_dict['__class__'], 'BaseModel',
+                         "__class__ key is 'BaseModel'")
+        self.assertEqual(instance_dict['id'], instance.id,
+                         "id in dict matches instance's id")
+        self.assertIsInstance(instance_dict['created_at'], str,
+                              "created_at is a string")
+        self.assertIsInstance(instance_dict['updated_at'], str,
+                              "updated_at is a string")
 
     def test_uuid_uniqueness(self):
         """Test the UUID uniqueness of BaseModel instances."""
         instance1 = BaseModel()
         instance2 = BaseModel()
-        self.assertNotEqual(instance1.id, instance2.id, "Each instance should have a unique id")
+        self.assertNotEqual(instance1.id, instance2.id,
+                            "Each instance has a unique id")
 
     def test_datetime_types(self):
         """Test the type of datetime attributes."""
         instance = BaseModel()
-        self.assertIsInstance(instance.created_at, datetime, "created_at should be a datetime object")
-        self.assertIsInstance(instance.updated_at, datetime, "updated_at should be a datetime object")
+        self.assertIsInstance(instance.created_at, datetime,
+                              "created_at is a datetime object")
+        self.assertIsInstance(instance.updated_at, datetime,
+                              "updated_at is a datetime object")
 
     def test_update_on_save(self):
-        """Test that 'save' method updates 'updated_at' without changing 'created_at'."""
+        """Test 'save' updates 'updated_at' without changing 'created_at'."""
         instance = BaseModel()
         old_created_at = instance.created_at
         old_updated_at = instance.updated_at
         instance.save()
-        self.assertEqual(instance.created_at, old_created_at, "'created_at' should not change on save")
-        self.assertNotEqual(instance.updated_at, old_updated_at, "'updated_at' should be updated on save")
+        self.assertEqual(instance.created_at, old_created_at,
+                         "'created_at' unchanged on save")
+        self.assertNotEqual(instance.updated_at, old_updated_at,
+                            "'updated_at' updated on save")
 
     def test_attribute_mutation(self):
-        """Test the addition and modification of attributes."""
+        """Test the addition & modification of attributes."""
         instance = BaseModel()
         instance.name = "Test Name"
         instance.number = 42
-        self.assertEqual(instance.name, "Test Name", "New attributes should be added correctly")
-        self.assertEqual(instance.number, 42, "New attributes should be added correctly")
+        self.assertEqual(instance.name, "Test Name",
+                         "New attributes added correctly")
+        self.assertEqual(instance.number, 42,
+                         "New attributes added correctly")
         instance.number = 43
-        self.assertEqual(instance.number, 43, "Attributes should be modifiable")
+        self.assertEqual(instance.number, 43,
+                         "Attributes modifiable")
 
     def test_invalid_kwargs_handling(self):
-        """Test initialization with both valid and invalid kwargs."""
+        """Test init with both valid & invalid kwargs."""
         kwargs = {
             'nonexistent_field': 'value',
             'id': '123',
@@ -94,8 +114,10 @@ class TestBaseModel(unittest.TestCase):
             'updated_at': '2021-01-02T00:00:00'
         }
         instance = BaseModel(**kwargs)
-        self.assertTrue(hasattr(instance, 'nonexistent_field'), "Instance should handle dynamic attribute assignment")
-        self.assertEqual(instance.id, '123', "Valid kwargs should be handled correctly")
+        self.assertTrue(hasattr(instance, 'nonexistent_field'),
+                        "Instance handles dynamic attribute assignment")
+        self.assertEqual(instance.id, '123',
+                         "Valid kwargs handled correctly")
 
 
 if __name__ == '__main__':
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,159 @@
 #!/usr/bin/python3
-"""Modules"""
+"""
+This module is a unittest for the FileStorage class
+"""
+import tempfile
+import shutil
 import unittest
-from ....models.base_model import BaseModel
+import os
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """Let's test"""
-    pass
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        FileStorage._FileStorage__file_path = os.path.join(
+            self.temp_dir, 'temp_file.json')
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        # Remove the temporary directory after the test
+        shutil.rmtree(self.temp_dir)
+
+    def test_all_method(self):
+        """Test the all method of FileStorage."""
+        all_objects = self.storage.all()
+        self.assertIsInstance(all_objects, dict,
+                              "all should return a dictionary")
+
+    def test_new_method(self):
+        """Test the new method of FileStorage."""
+        storage = FileStorage()
+        instance = BaseModel()
+        storage.new(instance)
+        all_objects = storage.all()
+        key = f"{instance.__class__.__name__}.{instance.id}"
+        self.assertIn(key, all_objects, "New object should be in storage")
+
+    def test_save_method(self):
+        """Test the save method of FileStorage."""
+        instance = BaseModel()
+        self.storage.new(instance)
+        self.storage.save()
+        with open(FileStorage._FileStorage__file_path, 'r') as file:
+            data = file.read()
+            self.assertIn(instance.id, data, "New object's id in saved file")
+
+    def test_reload_method(self):
+        """Test the reload method of FileStorage."""
+        storage = FileStorage()
+        instance = BaseModel()
+        storage.new(instance)
+        storage.save()
+        storage.reload()
+        all_objects = storage.all()
+        key = f"{instance.__class__.__name__}.{instance.id}"
+        self.assertIn(key, all_objects, "Saved object in reloaded storage")
+
+    def test_file_creation(self):
+        """Test file creation on save."""
+        self.storage.new(BaseModel())
+        self.storage.save()
+        self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path),
+                        "File created on save")
+
+    def test_save_with_multiple_objects(self):
+        """Test saving multiple objects."""
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        self.storage.new(instance1)
+        self.storage.new(instance2)
+        self.storage.save()
+
+        with open(FileStorage._FileStorage__file_path, 'r') as f:
+            content = f.read()
+            self.assertIn(instance1.id, content, "First object's id in file")
+            self.assertIn(instance2.id, content, "Second object's id in file")
+
+    def test_reload_with_multiple_objects(self):
+        """Test reloading multiple objects."""
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        self.storage.new(instance1)
+        self.storage.new(instance2)
+        self.storage.save()
+
+        self.storage.reload()
+        all_objects = self.storage.all()
+        self.assertIn(f"{instance1.__class__.__name__}.{instance1.id}",
+                      all_objects, "First object in reloaded storage")
+        self.assertIn(f"{instance2.__class__.__name__}.{instance2.id}",
+                      all_objects, "Second object in reloaded storage")
+
+    def test_object_deletion(self):
+        """Test deletion of an object."""
+        instance = BaseModel()
+        self.storage.new(instance)
+        key = f"{instance.__class__.__name__}.{instance.id}"
+        del self.storage.all()[key]  # Simulate deletion
+
+        self.storage.save()
+        self.storage.reload()
+        self.assertNotIn(key, self.storage.all(),
+                         "Deleted object not in storage after reload")
+
+    def test_object_update(self):
+        """Test object update."""
+        instance = BaseModel()
+        instance.name = "Test Name"
+        self.storage.new(instance)
+        self.storage.save()
+
+        instance.name = "Updated Name"
+        self.storage.save()
+        self.storage.reload()
+
+        reloaded_instance = self.storage.all()[
+            f"{instance.__class__.__name__}.{instance.id}"]
+        self.assertEqual(reloaded_instance.name, "Updated Name",
+                         "Updated attribute saved and reloaded")
+
+    def test_file_path_customization(self):
+        """Test customization of the file path."""
+        new_file_path = os.path.join(self.temp_dir, "test_file.json")
+        FileStorage._FileStorage__file_path = new_file_path
+        self.storage.save()
+
+        self.assertTrue(os.path.exists(new_file_path),
+                        "File created at new file path")
+        os.remove(new_file_path)
+
+    def test_data_integrity(self):
+        """Test data integrity after save and reload."""
+        instance = BaseModel()
+        instance.name = "Test Name"
+        self.storage.new(instance)
+        self.storage.save()
+
+        print("Current working directory:", os.getcwd())
+
+        self.storage.reload()
+        reloaded_instance = self.storage.all()[
+            f"{instance.__class__.__name__}.{instance.id}"]
+        self.assertEqual(reloaded_instance.name, "Test Name",
+                         "Data intact after save and reload")
+
+    def test_save_method_one(self):
+        """Test the save method of FileStorage."""
+        instance = BaseModel()
+        self.storage.new(instance)  # Add object
+        self.storage.save()
+
+        # Check if file is created
+        self.assertTrue(os.path.exists('file.json'), "File created on save")
+
+
+if __name__ == '__main__':
+    unittest.main()
